155f3d8549e212a30d1139faec566774c9df2b8a
ahmedsport <ahmedsport@hotmail.com>
2022-07-27 11:11:41 +0100

    for and foreach

--- Start/Program Flow/ForLoops/Program.cs
+++ Start/Program Flow/ForLoops/Program.cs
@@ -7,19 +7,40 @@ namespace ForLoops
         static void Main(string[] args)
         {
             int myVal = 15;
-            int[] nums = new int[] {3, 14, 15, 92, 6};
+            int[] nums = new int[] { 3, 14, 15, 92, 6 };
             string str = "The quick brown fox jumps over the lazy dog";
 
             // TODO: the basic for loop
-            Console.WriteLine("The basic for loop:");
+            // Console.WriteLine("The basic for loop:");
+            // for (int i = 0; i < myVal; i++)
+            // {
+            //     Console.WriteLine("i is currently is: {0}", i);
+            // }
 
             Console.WriteLine();
 
             // TODO: the foreach-in loop can be used to iterate over sequences
             // Console.WriteLine("The foreach loop:");
+            foreach (int num in nums)
+            {
+                Console.WriteLine("i is currently {0}", num);
+
+            }
 
 
             // TODO: count the number of o's in the string
+            int count = 0;
+            foreach (var c in str)
+            {
+                if (c == 'o')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("O appeared {0}", count);
+
+
+
 
         }
     }
