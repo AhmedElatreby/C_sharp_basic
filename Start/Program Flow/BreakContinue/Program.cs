7e1b6a243e6423551675e1ec1a557265d0a9e347
ahmedsport <ahmedsport@hotmail.com>
2022-07-27 12:52:24 +0100

    break and conuinue

--- Start/Program Flow/BreakContinue/Program.cs
+++ Start/Program Flow/BreakContinue/Program.cs
@@ -6,19 +6,26 @@ namespace BreakContinue
     {
         static void Main(string[] args)
         {
-            int[] values = {15, 7, 12, 23, 41, 28, 9, 17, 36};
+            int[] values = { 15, 7, 12, 23, 41, 28, 9, 17, 36 };
 
             Console.WriteLine("Using break and continue :");
             foreach (int val in values)
             {
                 // TODO: The continue statement skips the rest of the loop entirely
                 // and jumps to the next iteration (if there is one)
-
+                if (val >= 16 && val <= 30)
+                {
+                    continue;
+                }
 
                 // print the value
                 Console.WriteLine($"val is currently {val}");
 
                 // TODO: The break statement stops the loop and exits
+                // if (val >= 20)
+                // {
+                //     break; 
+                // }
 
             }
         }
