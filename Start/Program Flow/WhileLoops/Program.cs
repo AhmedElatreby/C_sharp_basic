27928c18a69270439fd0360e089e5c9f1258a1a2
ahmedsport <ahmedsport@hotmail.com>
2022-07-27 12:45:55 +0100

    while and do while loop

--- Start/Program Flow/WhileLoops/Program.cs
+++ Start/Program Flow/WhileLoops/Program.cs
@@ -10,11 +10,23 @@ namespace WhileLoops
 
             // TODO: basic while loop executes while the gate condition is true
             Console.WriteLine("Basic while() loop:");
+            // while (inputStr != "exit")
+            // {
+            //     inputStr = Console.ReadLine();
+            //     Console.WriteLine("you entered: {0}", inputStr);
+
+            // }
 
             Console.WriteLine();
 
             // TODO: the do-while loop always executes at least one time
             // Console.WriteLine("The  do-while() loop:");
+            do
+            {
+                inputStr = Console.ReadLine();
+                Console.WriteLine("you entered: {0}", inputStr);
+
+            } while (inputStr != "exit");
 
             // Console.WriteLine();
         }
