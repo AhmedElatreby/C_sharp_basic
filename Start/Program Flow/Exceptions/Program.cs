6ffe5afa4363a57da75e0303641c6534fd68e102
ahmedsport <ahmedsport@hotmail.com>
2022-07-27 13:49:50 +0100

    Exceptions

--- Start/Program Flow/Exceptions/Program.cs
+++ Start/Program Flow/Exceptions/Program.cs
@@ -6,13 +6,34 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
-            int x = 100;
-            int y = 10;
+            int x = 1002;
+            int y = 0;
             int result;
 
             // TODO: try-catch expressions make error checking easier
-            result = x / y;
-            Console.WriteLine("The result is: {0}", result);
+            try
+            {
+                if (x >= 1000)
+                {
+                    throw new ArgumentOutOfRangeException("x", "x has to be 100 or less");
+                }
+                result = x / y;
+                Console.WriteLine("The result is: {0}", result);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine($"invalid number!");
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Sorry 100 is the limit!");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine($"This line will always print");
+            }
         }
     }
 }
