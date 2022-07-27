179dd6a34dce8e198f62850a5d4009c04dfafff2
ahmedsport <ahmedsport@hotmail.com>
2022-07-27 10:20:22 +0100

    ternary operator

--- Start/Program Flow/Conditional-if/Program.cs
+++ Start/Program Flow/Conditional-if/Program.cs
@@ -6,9 +6,25 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            int theVal = 50;
+            int theVal = 51;
 
             // TODO: if-else 
+            if (theVal == 50)
+            {
+                Console.WriteLine($"the value is 50");
+
+            }
+            else if (theVal >= 51 && theVal <= 60)
+            {
+                Console.WriteLine($"The value is between 51 and 60 ");
+
+
+            }
+            else
+            {
+                Console.WriteLine($"the value is something else");
+
+            }
 
 
             // -----------------------
@@ -25,6 +41,8 @@ namespace Conditionals
             */
 
             // TODO: can be replaced by a ternary operator ?:
+            Console.WriteLine(theVal < 50 ? "the value is less than 50" : "the value is greater then 50");
+
 
         }
     }
