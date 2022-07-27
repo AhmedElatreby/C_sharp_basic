19394c69d275ba938a9a73c59d5b5d3611ddfb40
ahmedsport <ahmedsport@hotmail.com>
2022-07-27 10:35:24 +0100

    Switch

--- Start/Program Flow/Conditional-switch/Program.cs
+++ Start/Program Flow/Conditional-switch/Program.cs
@@ -6,9 +6,33 @@ namespace ConditionalOps
     {
         static void Main(string[] args)
         {
-            int theVal = 50;
+            int theVal = 54;
 
             // TODO: The switch statement
+            switch (theVal)
+            {
+                case 50:
+                    Console.WriteLine($"The value is 50");
+                    break;
+                case 51:
+                    Console.WriteLine($"The value is 51");
+                    break;
+                case 52:
+                    Console.WriteLine($"The value is 52");
+                    break;
+                case 53:
+                    Console.WriteLine($"The value is 53");
+                    break;
+                case 54:
+                case 55:
+                case 56:
+                    Console.WriteLine($"The value is between 54 and 56");
+                    break;
+                default:
+                    Console.WriteLine($"The value is over 56");
+                    break;
+            }
+
 
         }
     }
