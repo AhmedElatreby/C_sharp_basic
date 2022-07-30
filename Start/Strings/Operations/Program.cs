8cddce7b2a6713cb7a43852b67427513fa78bb98
ahmedsport <ahmedsport@hotmail.com>
2022-07-30 16:06:45 +0100

    String

--- Start/Strings/Operations/Program.cs
+++ Start/Strings/Operations/Program.cs
@@ -11,21 +11,36 @@ namespace Operations
             string str1 = "The quick brown fox jumps over the lazy dog.";
             string str2 = "This is a string";
             string str3 = "THIS is a STRING";
-            string[] strs = {"one", "two", "three", "four"};
+            string[] strs = { "one", "two", "three", "four" };
 
             // TODO: Length of a string 
-
+            Console.WriteLine(str1.Length);
 
             // TODO: Access individual characters
+            Console.WriteLine(str1[12]);
 
 
             // TODO: iterate over a string like any other sequence of values
-
+            foreach (char c in str1)
+            {
+                Console.Write(c);
+                if (c == 'j')
+                {
+                    Console.WriteLine();
+                    break;
+                }
+            }
 
             // TODO: String Concatenation         
-
+            outstr = String.Concat(strs);
+            Console.WriteLine(outstr);
 
             // TODO: Joining strings together with Join
+            outstr = String.Join('.', strs);
+            Console.WriteLine(outstr);
+
+            outstr = String.Join("---", strs);
+            Console.WriteLine(outstr);
 
 
             // TODO: String Comparison
@@ -33,12 +48,22 @@ namespace Operations
             // < 0 : first string comes before second in sort order
             // 0 : first and second strings are same position in sort order
             // > 0 : first string comes after the second in sort order
+            int result = String.Compare(str2, "This is a string");
+            Console.WriteLine(result);
 
 
             // TODO: Equals just returns a regular Boolean
-
+            bool isEqual = str2.Equals(str3);
+            Console.WriteLine(isEqual);
 
             // TODO: String Searching
+            Console.WriteLine(str1.IndexOf("dog"));
+            Console.WriteLine(str1.LastIndexOf("fox"));
+
+            outstr = str1.Replace("fox", "cat");
+            Console.WriteLine(outstr);
+
+
 
         }
     }
