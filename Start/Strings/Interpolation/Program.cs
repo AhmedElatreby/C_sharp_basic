bf9c4f70be864b9e60398f2df573d19f778da8cc
ahmedsport <ahmedsport@hotmail.com>
2022-07-30 18:57:52 +0100

    String interpolation

--- Start/Strings/Interpolation/Program.cs
+++ Start/Strings/Interpolation/Program.cs
@@ -14,12 +14,17 @@ namespace StringInterpolation
             decimal price = 60_275.0m;
 
             // TODO: Output information using formatting
-
+            Console.WriteLine("This car is a {0}, {1}, {2}, with {3} miles and cost ${4}",
+             year, make, model, miles, price);
 
             // TODO: Using string interpolation
 
+            Console.WriteLine($"This car is a {year}, {make}, {model}, with {miles} miles and cost £{price:N1}");
+
 
             // TODO: With inline expressions
+            Console.WriteLine($"This car is a {year}, {make}, {model}, with {{{miles * 1.6:n2}}} km and cost £{price:N1}");
+
 
         }
     }
