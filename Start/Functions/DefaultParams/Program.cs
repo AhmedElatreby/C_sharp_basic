01e80f145b0f4cdfd3dae7259ac21ff489e07190
ahmedsport <ahmedsport@hotmail.com>
2022-07-31 22:17:41 +0100

    Nmaed and default parameteres

--- Start/Functions/DefaultParams/Program.cs
+++ Start/Functions/DefaultParams/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 
 // TODO: Functions can provide default values for their parameters
-void PrintWithPrefix(string thestr, string prefix)
+void PrintWithPrefix(string thestr, string prefix = "") // ="" this will allow to not use the parameters 
 {
     Console.WriteLine($"{prefix} {thestr}");
 }
 
 // TODO: Test the default parameters
-
+PrintWithPrefix("Hello", "there!");
+PrintWithPrefix("Hello There!");
 
 // TODO: Call with named params
+PrintWithPrefix(prefix: "---", thestr: "Hello there!");
 
