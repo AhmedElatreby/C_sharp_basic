27f326569984c5b36c9cbb96d333c5f00389059b
ahmedsport <ahmedsport@hotmail.com>
2022-07-31 22:08:57 +0100

    basic function

--- Start/Functions/Basics/Program.cs
+++ Start/Functions/Basics/Program.cs
@@ -4,13 +4,24 @@
 // unit that can be customized with parameters.
 
 // TODO: Functions have a return type, name, and optional parameters
-
+float milesToKm(float miles)
+{
+    float result = miles * 1.6f;
+    return result;
+}
 
 // TODO: A function with no return value has a 'void' type
+void printWithPrefix(String str)
+{
+    Console.WriteLine($"::> {str} ");
+}
 
 
 // TODO: Call first function
-
+Console.WriteLine($"The result is {milesToKm(8.0f):N2}");
+Console.WriteLine($"The result is {milesToKm(52.0f):N2}");
 
 // TODO: Call second function
 
+printWithPrefix("Test string");
+printWithPrefix("Test another string");
