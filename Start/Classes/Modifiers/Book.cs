77fddfc457e195c6a6bcca2cafec5c4c0c7f1d1e
ahmedsport <ahmedsport@hotmail.com>
2022-08-01 13:13:19 +0100

    getter and setter

--- Start/Classes/Modifiers/Book.cs
+++ Start/Classes/Modifiers/Book.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Modifiers
 {
     // access modifiers control how properties and methods are accessed
@@ -7,27 +5,60 @@ namespace Modifiers
     {
         // TODO: "public" members and methods can be accessed by any other code
         // Note: this is *NOT* the right way to expose internal data
-        string _name;
+        public string _name;
 
         // TODO: "protected" members can only be accessed by the class or a 
         // derived class from this one
-        string _author;
+        private string _author;
 
         // TODO: "private" is the default and can only be accessed by code 
         // within the class itself
-        int _pagecount;
+        protected int _pagecount;
 
-        public Book(string name, string author, int pages) {
+        public Book(string name, string author, int pages)
+        {
             _name = name;
             _author = author;
             _pagecount = pages;
         }
 
-        public string GetDescription() {
+        public string GetDescription()
+        {
             return $"{_name} by {_author}, {_pagecount} pages";
         }
 
         // TODO: Member variables can be accessed via methods
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string s)
+        {
+            _name = s;
+        }
+
+        public string GetAuthor()
+        {
+            return _author;
+        }
+
+        public void SetAuthor(string s)
+        {
+            _author = s;
+        }
+
+        public int GetPageCount()
+        {
+            return _pagecount;
+        }
+
+        public void SetPageCount(int c)
+        {
+            _pagecount = c;
+        }
+
+
 
     }
 }
--- Start/Classes/Modifiers/Program.cs
+++ Start/Classes/Modifiers/Program.cs
@@ -15,6 +15,12 @@ namespace Modifiers
             Console.WriteLine(b1.GetDescription());
 
             // TODO: set data using functions instead of accessing fields directly
+            b1.SetName("A new day, a new life");
+            b1.SetAuthor("Tom Smith");
+            b1.SetPageCount(1000);
+
+            Console.WriteLine(b1.GetDescription());
+
 
         }
     }
