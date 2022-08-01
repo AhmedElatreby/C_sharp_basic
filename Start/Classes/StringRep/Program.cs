b525118e2005fb0e39bb352e477a6f0a1ee5a1e8
ahmedsport <ahmedsport@hotmail.com>
2022-08-01 16:47:47 +0100

    String representation

--- Start/Classes/StringRep/Program.cs
+++ Start/Classes/StringRep/Program.cs
@@ -8,15 +8,30 @@ namespace StringRep
         {
             // TODO: Every class in C# inherits from Object, which means
             // that every class inherits the ToString() method
-
+            int x = 100;
+            Console.WriteLine(x.ToString());
 
             // TODO: if you don't override the method, the default behavior just prints
             // the name of the class and the namespace
+            object a = new object();
+            Console.WriteLine($"{a}");
+
 
 
             // TODO: use the ToString method on the Book class
             Book b1 = new Book("War and Peace", "Leo Tolstoy", 825);
-            
+
+            Console.WriteLine($"{b1.ToString()}");
+            Console.WriteLine($"{b1}");     // this going to work the same as ToString
+
+            Console.WriteLine($"{b1.ToString('B')}"); // overload ToString
+            Console.WriteLine($"{b1.ToString('F')}"); // overload ToString
+            Console.WriteLine($"{b1.ToString()}"); // override ToString
+
+
+
+
+
         }
     }
 }
