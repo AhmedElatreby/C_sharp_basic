d30f5a8736ebd07e6c13e3fd4257bfd900c48bdd
ahmedsport <ahmedsport@hotmail.com>
2022-08-01 20:55:51 +0100

    Bank Account

--- Start/Classes/ProgChallenge/Program.cs
+++ Start/Classes/ProgChallenge/Program.cs
@@ -7,15 +7,15 @@ namespace ProgChallenge
         static void Main(string[] args)
         {
             // Create the Checking Account with initial balance
-            CheckingAcct checking = new CheckingAcct("John", "Doe", 2500.0m);
+            CheckingAcct checking = new CheckingAcct("Jon", "Smith", 2500.0m);
 
             // Create the Savings Account with interest and initial balance
-            SavingAcct saving = new SavingAcct("Jane", "Doe", 0.025m, 1000.0m);
+            SavingsAcct saving = new SavingsAcct("Jane", "Smith", 0.025m, 1000.0m);
 
             // Check the balances
             // Expected output should be 2500 and 1000 at this point
-            Console.WriteLine($"Checking balance is {checking.Balance:C2}");
-            Console.WriteLine($"Savings balance is {saving.Balance:C2}");
+            Console.WriteLine($"Checking balance is £{checking.Balance:N2}");
+            Console.WriteLine($"Savings balance is £{saving.Balance:N2}");
 
             // Print the account owner information. Expected output: 
             // "Checking owner: John Doe"
@@ -29,8 +29,8 @@ namespace ProgChallenge
 
             // Check the balances
             // Expected output should be 2700 and 1150 at this point
-            Console.WriteLine($"Checking balance is {checking.Balance:C2}");
-            Console.WriteLine($"Savings balance is {saving.Balance:C2}");
+            Console.WriteLine($"Checking balance is £{checking.Balance:N2}");
+            Console.WriteLine($"Savings balance is £{saving.Balance:N2}");
 
             // Make some withdrawals from each account
             checking.Withdraw(50.0m);
@@ -38,20 +38,20 @@ namespace ProgChallenge
 
             // Check the balances
             // Expected output should be 2650 and 1025 at this point
-            Console.WriteLine($"Checking balance is {checking.Balance:C2}");
-            Console.WriteLine($"Savings balance is {saving.Balance:C2}");
+            Console.WriteLine($"Checking balance is £{checking.Balance:N2}");
+            Console.WriteLine($"Savings balance is £{saving.Balance:N2}");
 
             // Apply the Savings interest
             saving.ApplyInterest();
             // Savings balance should now be 1050.63 
-            Console.WriteLine($"Savings balance is {saving.Balance:C2}");
+            Console.WriteLine($"Savings balance is £{saving.Balance:N2}");
 
             // More than 3 Savings withdrawals should result in 2.00 charge
             saving.Withdraw(10.0m);
             saving.Withdraw(20.0m);
             saving.Withdraw(30.0m);
             // Savings balance should now be 988.63
-            Console.WriteLine($"Savings balance is {saving.Balance:C2}");
+            Console.WriteLine($"Savings balance is £{saving.Balance:N2}");
 
             // try to overdraw savings - this should be denied and print message
             saving.Withdraw(2000.0m);
@@ -60,8 +60,8 @@ namespace ProgChallenge
             checking.Withdraw(3000.0m);
 
             // Expected output should be -385 and 988.63
-            Console.WriteLine($"Checking balance is {checking.Balance:C2}");
-            Console.WriteLine($"Savings balance is {saving.Balance:C2}");
+            Console.WriteLine($"Checking balance is £{checking.Balance:N2}");
+            Console.WriteLine($"Savings balance is £{saving.Balance:N2}");
         }
     }
 }
--- Start/Classes/ProgChallenge/SavingAcct.cs
+++ Start/Classes/ProgChallenge/SavingAcct.cs
@@ -2,8 +2,46 @@ using System;
 
 namespace ProgChallenge
 {
-    class SavingsAcct
+    class SavingsAcct : Account
     {
+        private int _withdrawacount = 0;
+        private const decimal WITHDRAW_CHARGE = 2.0m;
+        private const int WITHDRAW_LIMIT = 3;
+
+        public SavingsAcct(string fname, string lname, decimal interest, decimal initial) : base(fname, lname, initial)
+        {
+            InterestRate = interest;
+        }
+
+        public decimal InterestRate { get; set; }
+
+        public void ApplyInterest()
+        {
+            Balance += (Balance * InterestRate);
+        }
+
+        public override void Withdraw(decimal amount)
+        {
+            if (amount > Balance)
+            {
+                Console.WriteLine($"Attempt to overdraw saving - denied");
+
+            }
+            else
+            {
+                base.Withdraw(amount);
+
+                _withdrawacount++;
+                if (_withdrawacount > WITHDRAW_LIMIT)
+                {
+                    Console.WriteLine($"More than withdrawals - extra charge");
+                    base.Withdraw(WITHDRAW_CHARGE);
+
+                }
+            }
+        }
+
+
 
     }
 
