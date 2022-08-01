5eb7e1f3b9353a09e0c07d7ff5b6ecf9ab8ba504
ahmedsport <ahmedsport@hotmail.com>
2022-08-01 13:37:56 +0100

    Defining properties

--- Start/Classes/Properties/Book.cs
+++ Start/Classes/Properties/Book.cs
@@ -11,7 +11,8 @@ namespace Properties
         private int _pagecount;
 
         // classes have one or more constructors
-        public Book(string name, string author, int pages) {
+        public Book(string name, string author, int pages)
+        {
             _name = name;
             _author = author;
             _pagecount = pages;
@@ -19,19 +20,53 @@ namespace Properties
 
         // TODO: use Properties to implement access to our internal data
         // This is called a Property with a "backing field"
-
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
 
         // TODO: There's a shorthand way of writing these using the => operator
         // to create "expression-bodied" properties
+        public string Author
+        {
+            get => _author;
+            set => _author = value;
+        }
 
+        public int Pagecount
+        {
+            get => _pagecount;
+            set => _pagecount = value;
+        }
 
         // TODO: You can omit the get or set if you want to prevent
         // modification or reading the value, or if you want to create
         // a "computed property" from other fields
-
+        public string Description
+        {
+            get => $"{Name} by {Author}, {Pagecount} pages";
+        }
 
         // TODO: Properties can be auto-generated - if there's no
         // backing field, the property can hold the data
+        public string ISBN
+        {
+            get;
+            set;
+        }
+
+        public decimal Price
+        {
+            get;
+            set;
+        }
 
     }
 }
