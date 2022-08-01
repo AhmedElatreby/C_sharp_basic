450182bb2829468c3acc3efd43336c3ec00d3c4e
ahmedsport <ahmedsport@hotmail.com>
2022-08-01 12:54:45 +0100

    classes

--- Start/Classes/Defining/Book.cs
+++ Start/Classes/Defining/Book.cs
@@ -6,12 +6,24 @@ namespace Defining
     public class Book
     {
         // TODO: classes have member variables, or "fields" to hold data
-
+        string _name;
+        string _author;
+        int _pageCount;
 
         // TODO: classes have one or more constructors
-
+        public Book(string name, string author, int pages)
+        {
+            _name = name;
+            _author = author;
+            _pageCount = pages;
+        }
 
         // TODO: methods are used to operate on the class and data
+        // you cannot change the variables here
+        // public string GetDescription()
+        // {
+        //     return $"{_name} by {_author}";
+        // }
 
     }
 }
--- Start/Classes/Defining/Program.cs
+++ Start/Classes/Defining/Program.cs
@@ -7,13 +7,16 @@ namespace Defining
         static void Main(string[] args)
         {
             // TODO: Create new object instances using the "new" operator
+            Book b1 = new Book("All about C#", "someone", 567);
+            Book b2 = new Book("The best about C#", "someone", 569);
 
-            
             // TODO: Call a method on the object
-
+            Console.WriteLine(b1.GetDescription());
+            Console.WriteLine(b2.GetDescription());
 
             // TODO: try to set one of the properties -- 
             // this will result in an error
+            b1._name = "something new";
 
         }
     }
